47dea265e60721496d877d34620cb0a6c04ac1e8
Elian Xavier <45861861+elianxavier@users.noreply.github.com>
2025-06-21 11:13:17 -0300

    Implement user and task management features with UserController, TaskController, and TaskModel

--- src/main/java/br/com/elianxavier/todolist/controller/user/UserController.java
+++ src/main/java/br/com/elianxavier/todolist/controller/user/UserController.java
@@ -1,8 +1,11 @@
 package br.com.elianxavier.todolist.controller.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +33,10 @@ public class UserController {
 
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+    @GetMapping("/list")
+    public List<UserModel> list() {
+        return this.userRepository.findAll();
     }
 }
